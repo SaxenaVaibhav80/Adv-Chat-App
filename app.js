6ff0ecc0c234e279d7a3e971f2ebf085359741ff
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-10-31 16:00:06 +0530

    login  with jwt token and signup done

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ const http=require('http')
 const { createServer } = require("http2")
 const app= express()
 const webpush=require('web-push')
+const mongoose = require('mongoose')
 require('dotenv').config();
 const secret_key=process.env.SECRET_KEY
 const bodyParser= require("body-parser")
 const jwt= require("jsonwebtoken")
+const multer = require("multer");
+const grid = require("gridfs-stream")
+const { GridFsStorage } = require("multer-gridfs-storage");
+const path = require("path");
+const crypto = require("crypto");
+const { MongoClient, ObjectId } = require("mongodb");
 const cookieparser = require("cookie-parser")
 app.use(cookieparser());
 app.use(bodyParser.urlencoded({extended:true}))
-// const chatModel = require("./models/chat")
 const server = http.createServer(app)
 const socketio=require('socket.io');
 const io=socketio(server)
 const bcrypt= require('bcrypt')
 const ejs= require("ejs")
-const db = require("./config/config")
 const userModel=require("./models/user");
 const chatModel = require("./models/chat")
 const urlencoded = require("body-parser/lib/types/urlencoded");
@@ -28,16 +33,17 @@ const mongouri= process.env.URL
 const public= "BBXq7Er9eQirt4Q5UAoOT6jlbGe1coPPTSARDeOBur_CMV_pgn095iyHhmSvbZsNqfeN4vK-sum2bQ8jAIwY3oU"
 const private ="ZbndEMVQv8KKtr0vPFNyxvhQsIU7r4G3XSZBsbu5uyA"
 
-const conn = mongoose.createConnection(mongouri);
+mongoose.connect(mongouri).then(() => {
+    console.log("MongoDB connected successfully");
+    bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, { bucketName: "uploads" });
+    console.log("GridFSBucket initialized");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
 
 let bucket;
 
-conn.once("open", () => {
-    console.log(conn.db)
-    bucket = new mongoose.mongo.GridFSBucket(conn.db, { bucketName: "uploads" });
-    console.log("GridFSBucket initialized");
-});
-
+// making store for uploading the files---->
 
 const storage = new GridFsStorage({
     url: mongouri,
@@ -49,9 +55,10 @@ const storage = new GridFsStorage({
                 if (err) {
                     return reject(err);
                 }
+
                 const filename = buf.toString("hex") + path.extname(file.originalname);
                 const fileInfo = {
-                    user_id:user._id,
+                    metadata:{user_id:req.user.id},
                     filename: filename,
                     bucketName: "uploads",
                 };
@@ -63,6 +70,8 @@ const storage = new GridFsStorage({
 
 const upload = multer({ storage });
 
+// signup handler----->
+
 app.post("/signup",async(req,res)=>
     {
      const fname= req.body.fname
@@ -70,29 +79,30 @@ app.post("/signup",async(req,res)=>
      const password= req.body.password
      const email=req.body.email
     
-     
+    console.log(fname,lname,password,email)
      if(!(fname && lname && email && password))
      {
          res.status(400).send("all field are required")
      }
-     const exist = await userModel.findOne({Email:email})
+     const exist = await userModel.findOne({email:email})
      if(exist){
          res.status(401).send("user already exist")
      }
      else{
+
         const encpass= await bcrypt.hash(password,10)
         const user = await userModel.create({
             firstname:fname,
             lastname:lname,
             password:encpass,
-            email:email
+            Email:email
         })
       
      }
-        
-    
      res.redirect("/")
     })
+
+// sending token to the client so that it will be store in the localstorage--->
 
 app.post("/login/api",(req,res)=>
     {
@@ -109,12 +119,14 @@ app.post("/login/api",(req,res)=>
         
     })
 
+// longin handler----->
+
 app.post("/login",async(req,res)=>
     {
         const email= req.body.email
         const password= req.body.password
         const user = await userModel.findOne({email:email})
-        
+        console.log(email,password)
         if(user)
         {
             const passverify= await bcrypt.compare(password,user.password)
@@ -141,7 +153,8 @@ app.post("/login",async(req,res)=>
             res.status(400).send("user not  Available")
         }
     })
-        
+
+// checking for login---->
 
 const checkLoginState = (req, res, next) => {
     const token = req.cookies.token;
@@ -160,19 +173,166 @@ const checkLoginState = (req, res, next) => {
     next();
 };
 
+// uploading handler---->
+
+app.post("/upload", (req, res) => {
+    const token=req.cookies.token
+    if(token)
+    {
+        const verify = jwt.verify(token,secret_key)
+        const id = verify.id
+    }
+    req.user = {id:id}; 
+
+    upload.single("file")(req, res, (err) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.redirect("/");
+    });
+});
+
+// handling subscribe post request--->
+
+app.post("/subscribe", async (req, res) => {
+ 
+    const subscription=req.body.sub
+    const token=req.body.token
+  
+    try {
+    
+      const verification = jwt.verify(token, secret_key);
+      const id = verification.id;
+  
+      const user = await userModel.findOne({ _id: id });
+  
+      if (user) {
+        const users=await userModel.findOneAndUpdate(
+          { _id: id },
+          { subscription: subscription },
+          { new: true }
+        );
+      } else {
+        res.status(404).send("User not found.");
+      }
+    } catch (error) {
+      console.error("Error in subscription:", error);
+      res.status(400).send("Invalid token or server error.");
+    }
+  });
+
+// socket handler--->
+
+io.on("connection", (socket) => {
+    socket.on("token", async (token) => {
+      try {
+        if (token !== "undefined") {
+          const isvalid = jwt.verify(token, secret_key);
+  
+          if (isvalid) {
+            const id = isvalid.id;
+            const status = "online";
+            socket.join(id);
+            socket.emit("online", status);
+   
+            
+            socket.on("sendMessageToUser", async ({ userId, message }) => {
+              console.log(userId)
+              if (userId && message) {
+                console.log(userId)
+                app.post("/getNotification",async(req,res)=>
+                {
+                  const uid = req.body.uid
+                  const user = await userModel.findOne({_id:uid})
+                  const subofuser= user.subscription
+                  console.log(uid)
+                  payload=JSON.stringify({
+                      data:user
+                  })
+                  await webpush.sendNotification(subofuser, payload)
+                  .then(() => console.log("Push notification sent successfully."))
+                  .catch(err => console.error("Error sending push notification:", err));
+                })
+                  
+                socket.to(userId).emit("message", [message,id]);
+
+                
+                let chat = await chatModel.findOne({
+                  $or: [
+                    { senderId: id, receiverId: userId },
+                    { senderId: userId, receiverId: id }
+                  ]
+                });
+  
+                if (chat) {
+                  chat.message.push({
+                    senderId: id,
+                    receiverId: userId,
+                    text: message,
+                    timestamp: Date.now()
+                  });
+                  await chat.save();
+                } else {
+                  chat = await chatModel.create({
+                    senderId: id,
+                    receiverId: userId,
+                    message: [
+                      {
+                        senderId: id,
+                        receiverId: userId,
+                        text: message,
+                        timestamp: Date.now()
+                      }
+                    ]
+                  });
+                }
+              }
+            });
+
+            socket.on('load chat', async (uid) => {   
+                let loadchat = await chatModel.findOne({
+                    $or: [
+                      { senderId: id, receiverId: uid },
+                      { senderId: uid, receiverId: id }
+                    ]
+                });
+                if (loadchat) {
+                    socket.emit("Load msg", loadchat.message);
+                } else {
+                    socket.emit("Load msg", "start chat");
+                }
+            });
+            
+          }
+        }
+      } catch (err) {
+        socket.emit("offline", "offline");
+      }
+    });
+});
+
+// landing page Route-->
+
 app.get("/",(req,res)=>
 {
     res.render("landingPage")
 })
 
-// app.get("/chat",(req,res)=>
-// {
-//     res.render("/chat")
-// })
+// chat route page---->
+
+app.get("/chat",(req,res)=>
+{
+    res.render("/chat")
+})
+
+// signup route--->
+
 app.get("/signup",(req,res)=>
 {
     res.render("signup")
 })
+
+// login  route ---->
 
 app.get("/login",(req,res)=>
 {
